331416bb82e492c36f0d2dfee3f3fbe6d0d70785
padmanabhan-s <padmanabhankum@gmail.com>
2021-10-09 12:05:54 +0530

    moved procfile into the mysite

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -12,11 +12,7 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-if path not in sys.path:
-    sys.path.append(path)
-    
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
